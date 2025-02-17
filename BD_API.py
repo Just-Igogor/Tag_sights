89e936474284de68b683f87b33eb1def056d148d
Смелый <149298913+Just-Igogor@users.noreply.github.com>
2025-02-17 14:37:20 +0300

    Update BD_API.py

--- BD_API.py
+++ BD_API.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 
 def get_db_connection():
-    connection = sqlite3.connect("C:/Users/igogo/Desktop/Sol/taganrog_sights.db")
+    connection = sqlite3.connect("C:/Users/taganrog_sights.db")
     connection.row_factory = sqlite3.Row
     return connection
 
